d6313f08a0b69100d12cbbc0c18657501238cfa9
nathaniEL mujesia <nathanielmujesia@gmail.com>
2023-12-11 05:34:34 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+function writeCards(names, eventName){
+
+    const messages =[];
+for(let i =0;i <= names.length -1;++i){
+
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+
+    
+
+}
+return messages;
+}
+
+function countDown(num){
+    let i = num;
+while(num >= 0 && i >= 0){
+
+    console.log(i);
+    --i;
+}
+}
